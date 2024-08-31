e5972fd2387ecff4f82a99cd0074d562725387c3
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-31 23:09:28 +0300

    feature/Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {Route, Routes} from "react-router-dom";
-import Button from "./components/Buttons/Button.jsx";
+import Category from "./components/Category/Category.jsx";
+import React from "react";
+import Cup from '../src/assets/icons/cup.svg?react'
 
 function onClick() {
     console.log("clicked");
@@ -8,7 +10,7 @@ function onClick() {
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Button to="/" value='Send' onClick={onClick}/>}/>
+            <Route path="/" element={<Category Icon={Cup} text='Engine'/>}/>
             <Route path="*" element={<p>Something went wrong</p>}/>
         </Routes>
 
--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import css from './Buttons.module.css';
 
 function Button({type = 'submit', value, onClick}) {
