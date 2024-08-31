cb1dba4e2071e293e7d86a413ea4bc6f2327cec8
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-09-01 01:21:52 +0300

    Add NotFound component for 404 error handling
    - Import NotFound component in App.jsx and replace error message with it.
    - Create NotFound component to display a 404 image.
    - Add styling for the NotFound component in NotFound.module.css.
    - Include 404.jpg image in assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Category from "./components/Category/Category.jsx";
-import React from "react";
 import Cup from '../src/assets/icons/cup.svg?react'
-
-function onClick() {
-    console.log("clicked");
-}
+import NotFound from "./components/NotFound/NotFound.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Category Icon={Cup} text='Engine'/>}/>
-            <Route path="*" element={<p>Something went wrong</p>}/>
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
 
     )
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import css from './NotFound.module.css'
+import NotFoundImg from "../../assets/img/404.jpg";
+
+function NotFound() {
+    return (
+        <img src={NotFoundImg} alt="Not found" className={css['not-found']}/>
+    );
+}
+
+export default NotFound;
